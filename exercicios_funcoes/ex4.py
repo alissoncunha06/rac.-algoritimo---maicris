1bb01336365d94d3bad1d98f0662d831d165d857
alissoncunha06 <158228524+alissoncunha06@users.noreply.github.com>
2026-05-13 10:50:24 -0300

    ex4, apenas coloquei a def cadatro

--- exercicios_funcoes/ex4.py
+++ exercicios_funcoes/ex4.py
@@ -4,3 +4,22 @@ contatos em uma agenda telefônica, podendo excluir estes
 contatos. Deve ser exibido um menu com as opções: inserir,
 remover e sair.
 """
+agenda = {}
+
+def exibir_menu():
+    pass
+
+def cadastrar_contatos():
+    while True:
+        nome = input("Digite o nome do contato (ou 0 para sair): ")
+        if nome == "0":
+            break
+        try:
+            telefone = int(input("Digite o telefone do contato com o codigo do país e área juntos, ex: 5541992419905: "))
+            agenda[nome] = telefone
+        except ValueError:
+            print("Número inválido")
+    
+
+def remover_contatos():
+    pass
