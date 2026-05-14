84ab04dbecd6e938400d8fdde1138070bfb43366
alissoncunha06 <arcrodrigo07@gmail.com>
2026-05-14 20:59:07 -0300

    Update listasMatriz.py

--- rascunhos.py/listasMatriz.py
+++ rascunhos.py/listasMatriz.py
@@ -4,6 +4,7 @@ galera = [["João", 20], ["Pedro", 30], ["Jonas", 15], ["Lucas", 9]]
 print(galera)
 print("=-=" * 20)
 
-
+for pessoa in galera:
+  print(f"Pessoa: {pessoa[0]}, idade: {pessoa[1]}")
 
 print()
