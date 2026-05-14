4a82361ae9999b0c68f5a9288592507682ff4976
alissoncunha06 <158228524+alissoncunha06@users.noreply.github.com>
2026-05-14 09:03:56 -0300

    adicionei o ex4

--- exerciciosLista.py/ex4.py
+++ exerciciosLista.py/ex4.py
@@ -1,8 +1,16 @@
 print('=-' * 20)
-lista = ['a', 'b', 'c', 'd', 'e']
+lista = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 def contar(lista):
+    vogais = ['a', 'e', 'i', 'o', 'u']
+    lista2 = []
     contador = 0
     for i in lista:
-        if i == 'aeiou':
-            
+        if i in vogais:
+            contador += 1
+            lista2.append(i)
+    return contador, lista2
+
+contador, vogais_encontradas = contar(lista)
+print(f'Na lista {lista} existem {contador} vogais: {vogais_encontradas}')
+print('=-' * 20)
