8cf6bec9bd2888977a803165cf558c9c06053e26
alissoncunha06 <158228524+alissoncunha06@users.noreply.github.com>
2026-05-14 12:05:44 -0300

    Update ex9.py

--- exerciciosLista.py/ex9.py
+++ exerciciosLista.py/ex9.py
@@ -1,6 +1,6 @@
 vetor = []
 
-for i in range(2):
+for i in range(10):
     while True:
         try:
             num = int(input("Digite um número inteiro: "))
@@ -15,4 +15,20 @@ print(vetor)
 def conta(vetor):
     linha = []
     for num in vetor:
-        
+        linha.append(num * num)
+    return linha
+quadrados = conta(vetor)
+
+def somar(quadrados):
+    soma = 0
+    for num in quadrados:
+        soma += num
+    return soma
+
+soma_quadrados = somar(quadrados)
+print("=-=" * 10, "QUADRADOS DOS NÚMEROS", "=-=" * 10)
+print(quadrados)
+
+print("=-=" * 10, "SOMA DOS QUADRADOS", "=-=" * 10)
+print(f"Soma dos quadrados: {soma_quadrados}")
+
