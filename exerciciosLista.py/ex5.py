5e4c683c69d3e4b3986e2647f608b33732fb7b15
alissoncunha06 <158228524+alissoncunha06@users.noreply.github.com>
2026-05-14 09:19:36 -0300

    update ex5

--- exerciciosLista.py/ex5.py
+++ exerciciosLista.py/ex5.py
@@ -1,11 +1,30 @@
+print("=-=" * 50)
 inteiros = []
 
-while True:
-    for _ in range(10):    
-        try:
-            num = int(input("Digite um número inteiro: "))
-            inteiros.append(num)
-        except ValueError:
-            print("Entrada inválida, insira um número inteiro.")
-
-            
+for _ in range(10):    
+    try:
+        num = int(input("Digite um número inteiro: "))
+        inteiros.append(num)
+    except ValueError:
+        print("Entrada inválida, insira um número inteiro.")
+print()
+def lista_pares(inteiros):
+    pares = []
+    impares = []
+    for num in inteiros:
+        if num % 2 == 0:
+            pares.append(num)
+        elif num % 2 != 0:
+            impares.append(num)
+    return pares, impares
+numeros_pares, numeros_impares = lista_pares(inteiros)
+print("=-=" * 50)
+print(f"Na lista {inteiros} temos os seguintes números pares:")
+for numero in numeros_pares:
+    print(f"{numero}...", end="")
+print()
+print(f"E os seguintes números ímpares:")  
+for numero in numeros_impares:
+    print(f"{numero}...", end="")
+print()
+print("=-=" * 50)
