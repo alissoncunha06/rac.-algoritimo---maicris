07764e7a0d6f1b3d5b3d65d8678e4edb90ac76ad
alissoncunha06 <arcrodrigo07@gmail.com>
2026-05-14 23:00:37 -0300

    Update ex12.py

--- exerciciosLista.py/ex12.py
+++ exerciciosLista.py/ex12.py
@@ -6,25 +6,37 @@ Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine
 alunos = []
 dados = []
 
-dados.append(str(input("Aluno: ")))
-while True:
-  try:
-    idade = int(input("Idade:"))
-    dados.append(idade)
-  except ValueError:
-    print("Formato inválido, tente novamente (ex: 15)")
+for _ in range(3):
+  dados.append(str(input("Aluno: ")))
+  while True:
+    try:
+      idade = int(input("Idade: "))
+      dados.append(idade)
+      break
+    except ValueError:
+      print("Formato inválido, tente novamente (ex: 15)")
+    
+  while True:
+    try:
+      altura = int(input("altura em cm (ex:180): "))
+      dados.append(altura)
+      break
+    except ValueError:
+      print("Formato inválido, tente novamente (ex: 180)")
 
 alunos.append(dados[:])
 dados.clear()
 
 def media(alunos):
   altura = []
-  for i in alunos:
-    altura.append(i[1])
+  for p in alunos:
+    altura.append(p[1])
   return sum(altura) / len(altura)
 
 media_altura = media(alunos)
 
+# [0] é nome, [1] é idade e [2] é altura
+
 for pessoa in alunos:
-  if pessoa[1] < media_altura:
-    print(f"Aluno {aluno[0]} tem altura inferior a média {media_altura}")
+  if pessoa[1]
+
