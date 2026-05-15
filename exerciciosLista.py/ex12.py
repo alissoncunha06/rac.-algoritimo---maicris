4a711b77ebf17fdd2e11c93da595cf3142e62868
alissoncunha06 <arcrodrigo07@gmail.com>
2026-05-14 23:17:10 -0300

    Update ex12.py

--- exerciciosLista.py/ex12.py
+++ exerciciosLista.py/ex12.py
@@ -2,6 +2,7 @@
 Foram anotadas as idades e alturas de 30 alunos. Faça um Programa que determine quantos alunos com mais de
 13 anos possuem altura inferior à média de altura desses alunos.
 """
+print()
 
 alunos = []
 dados = []
@@ -23,20 +24,23 @@ for _ in range(3):
       break
     except ValueError:
       print("Formato inválido, tente novamente (ex: 180)")
-
-alunos.append(dados[:])
-dados.clear()
+  alunos.append(dados[:])
+  dados.clear()
 
 def media(alunos):
   altura = []
   for p in alunos:
-    altura.append(p[1])
+    altura.append(p[2])
   return sum(altura) / len(altura)
 
 media_altura = media(alunos)
 
 # [0] é nome, [1] é idade e [2] é altura
-
+print(alunos)
 for pessoa in alunos:
-  if pessoa[1]
+  if pessoa[1] > 13:
+    if pessoa[2] < media_altura:
+      print(f"{pessoa[0]} tem a altura de {pessoa[2]} inferior a média {media_altura:.1f}")
+print(f"\nTotal: {contador} aluno(s) com mais de 13 anos e altura abaixo da média")
 
+print()
