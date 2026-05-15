2c0dcf86b0746696b4bcffb9467464fcb3d71160
alissoncunha06 <arcrodrigo07@gmail.com>
2026-05-14 22:33:24 -0300

    Update listamatriz2.py

--- rascunhos.py/listamatriz2.py
+++ rascunhos.py/listamatriz2.py
@@ -4,6 +4,9 @@ for c in range(3):
   dado.append(str(input("Nome: ")))
   dado.append(int(input("Idade: ")))
   galera.append(dado[:])
-
+  dado.clear()
 
 print(galera)
+
+for p in galera:
+  print(f"{p[0]} tem {p[1]} anos")
