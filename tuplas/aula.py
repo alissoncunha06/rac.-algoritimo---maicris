23ceb9f4a4f7ced307c9f1112fa05334d515302c
alissoncunha06 <arcrodrigo07@gmail.com>
2026-05-16 16:49:54 -0300

    Update aula.py

--- tuplas/aula.py
+++ tuplas/aula.py
@@ -1,3 +1,10 @@
 """
-aula sobre tuplas, usa-se variáveis compostas. 
+aula sobre tuplas, usa-se variáveis compostas, as tuplas são imutáveis.
 """
+print()
+lanche = ("hamburger", "suco", "pizza", "pudim")
+
+for i in range(len(lanche)):
+  print(f"{i}- {lanche[i]}")
+
+print()
